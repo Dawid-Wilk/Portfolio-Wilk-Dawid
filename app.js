1761f3381a2d107ad5059bf622e57816659bdb82
Dawid-Wilk <wilkdawid.1995@gmail.com>
2022-04-04 17:30:18 +0200

    Navbar hamburger
    Made responsive navbar with hamburger icon

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.8 });
-tl.to(".slider", { y: "-100%", duration: 1.5, delay: 0.5 });
-tl.to(".intro", { y: "-100%", duration: 1.5 }, "-=1.5");
+tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
-tl.fromTo(".main-text", { opacity: 0 }, { opacity: 1, duration: 2 });
+tl.fromTo(".main-text", { opacity: 0 }, { opacity: 1, duration: 1.5 });
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+  })
+);
