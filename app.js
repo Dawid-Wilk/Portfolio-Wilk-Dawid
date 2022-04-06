af33ef38dddc93766733232bcf6c16089b851541
Dawid-Wilk <wilkdawid.1995@gmail.com>
2022-04-06 16:08:03 +0200

    Add about section
    New section with css styles

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Intro animation
+
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
 tl.to(".text", { y: "0%", duration: 1, stagger: 0.8 });
@@ -5,6 +7,8 @@ tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 });
 tl.to(".intro", { y: "-100%", duration: 1 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".main-text", { opacity: 0 }, { opacity: 1, duration: 1.5 });
+
+// Responsive navigation
 
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
